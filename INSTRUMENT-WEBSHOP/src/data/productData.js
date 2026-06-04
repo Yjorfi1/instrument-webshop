e27e95d254f14c04b20c6d892266c63b9dd638ca
yjorfi1 <yjorfi@hotmail.com>
2026-06-04 11:36:25 +0200

    Productlijst gemaakt + in app.jsx gezet

---
+++ INSTRUMENT-WEBSHOP/src/data/productData.js
@@ -0,0 +1,143 @@
+export const products = [
+{
+    id: 1,
+    title: "Grand Piano",
+    price: 4.999,
+    category: "percussion-instrument, string-instrument",
+    image: "./public/img/piano.png"
+},
+{
+    id: 2,
+    title: "Guitar",
+    price: 299.99,
+    category: "string-instrument",
+    image: "./public/img/guitar.png"
+},
+{
+    id: 3,
+    title: "Clarinet",
+    price: 149.99,
+    category: "wind-instrument",
+    image: "./public/img/clarinet.png"
+},
+{
+    id: 4,
+    title: "Electric Guitar",
+    price: 749.99,
+    category: "string-instrument",
+    image: "./public/img/electric-guitar.png"
+},
+{
+    id: 5,
+    title: "Trumpet",
+    price: 849.99,
+    category: "wind-instrument",
+    image: "./public/img/trumpet.png"
+},
+{
+    id: 6,
+    title: "Bell",
+    price: 24.99,
+    category: "percussion-instrument",
+    image: "./public/img/bell.png"
+},
+{
+    id: 7,
+    title: "Violin",
+    price: 629.99,
+    category: "string-instrument",
+    image: "./public/img/violin.png"
+},
+{
+    id: 8,
+    title: "Accordion",
+    price: 529.99,
+    category: "wind-instrument",
+    image: "./public/img/accordion.png"
+},
+{
+    id: 9,
+    title: "Bass Guitar",
+    price: 199.99,
+    category: "string-instrument",
+    image: "./public/img/bass-guitar.png"
+},
+{
+    id: 10,
+    title: "Drums",
+    price: 399.99,
+    category: "percussion-instrument",
+    image: "./public/img/drums.png"
+},
+{
+    id: 11,
+    title: "Handpan",
+    price: 499.99,
+    category: "percussion-instrument",
+    image: "./public/img/handpan.png"
+},
+{
+    id: 12,
+    title: "Harmonica",
+    price: 42.99,
+    category: "wind-instrument",
+    image: "./public/img/harmonica.png"
+},
+{
+    id: 13,
+    title: "Wind Chime",
+    price: 249.99,
+    category: "percussion-instrument",
+    image: "./public/img/wind-chime.png"
+},
+{
+    id: 14,
+    title: "Harp",
+    price: 6.999,
+    category: "string-instrument",
+    image: "./public/img/harp.png"
+},
+{
+    id: 15,
+    title: "Saxophone",
+    price: 489.99,
+    category: "wind-instrument",
+    image: "./public/img/saxophone.png"
+},
+{
+    id: 16,
+    title: "Triangle",
+    price: 429.99,
+    category: "percussion-instrument",
+    image: "./public/img/triangle.png"
+},
+{
+    id: 17,
+    title: "Kalimba",
+    price: 49.99,
+    category: "percussion-instrument",
+    image: "./public/img/kalimba.png"
+},
+{
+    id: 18,
+    title: "Cymbal",
+    price: 129.99,
+    category: "percussion-instrument",
+    image: "./public/img/cymbal.png"
+},
+{
+    id: 19,
+    title: "Chinese Gong",
+    price: 1249.99,
+    category: "percussion-instrument",
+    image: "./public/img/chinese-gong.png"
+},
+{
+    id: 20,
+    title: "Ukulele",
+    price: 569.99,
+    category: "string-instrument",
+    image: "./public/img/ukulele.png"
+}
+
+];
